ebe339f9244dd153f56c901f92baf2a5b2b9c3d2
Sam Avery <sam.avery1@gmail.com>
2022-08-30 14:53:12 -0500

    commit tree ad943edb40048a7d6704feaf7c93ab2d96af8544

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+    let cards = []
+    for (let i = 0; i <names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cards
+}
+
+
+function countDown(){
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
